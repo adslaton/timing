62d3bf831d0cce6c429bdec93e5ff5b75feb614f
A.D. Slaton <ad.slaton@turner.com>
2016-08-29 13:35:24 -0400

    setting up metrics object to include performance timing, host information, and DOMContentLoaded calculations

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Timing
 
 A client timing module to passes timing data to an endpoint
+
+## Example
+
+1. Run your favorite static server
+2. Load /example/index.html
+3. See the DOMContentLoaded output
--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,64 @@
 'use strict';
 
-function getDomContentLoaded(timing) {
-    var complete = timing.domComplete - timing.domContentLoadedEventStart;
+function getDomContentLoaded(perf) {
+    var complete = perf.domComplete - perf.navigationStart;
 
-    console.log('Load time: ' + complete + 'ms');
+    if (window.debug) {
+        console.log(document.readyState);
+        console.log(perf);
+        console.log('Page load time: ' + complete + 'ms');
+    }
+
+    return complete;
 }
 
-function initChecks() {
-    var timing = (window.performance && window.performance.timing) ? window.performance.timing : {};
-    console.log(timing);
-    getDomContentLoaded(timing);
+function postMetrics(metrics) {
+    if (metrics && window.timingPostUrl) {
+        var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance
+        xmlhttp.open('POST', window.timingPostUrl);
+        xmlhttp.setRequestHeader('Content-Type', 'application/json');
+        xmlhttp.send(JSON.stringify(metrics));
+    }
 }
 
-window.onload = initChecks;
+function getMetrics() {
+    var now = new Date(),
+        metrics = {
+            timing: {
+                performance: ((window.performance && window.performance.timing) ? window.performance.timing : {}),
+                location: {
+                    hash: window.location.hash,
+                    host: window.location.host,
+                    href: window.location.href,
+                    origin: window.location.origin,
+                    pathname: window.location.pathname,
+                    search: window.location.search
+                },
+                date: {
+                    formatted: now,
+                    ts: now.getTime()
+                }
+            },
+            complete: ''
+        },
+        debug = (window.location.search.indexOf('debug=true') > -1) || false;
+
+    window.debug = debug;
+
+    if (window.debug) {
+        console.log('Performance timing:');
+        console.log(metrics.timing);
+    }
+
+    metrics.complete = getDomContentLoaded(metrics.timing.performance);
+
+    postMetrics(metrics);
+
+    return metrics;
+}
+
+if (window.addEventListener) {
+    window.addEventListener('load', getMetrics);
+} else {
+    window.attachEvent('onload', getMetrics);
+}
